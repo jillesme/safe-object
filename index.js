ac55606fb63552c1dc8645a9b138b573e2b7c8b8
Jilles Soeters <jsoeters@xebia.com>
2015-11-26 20:07:55 +0100

    Fix npm noobyness

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-module.exports = {
-  safe: function (root, children) {
-    var props = children.split('.');
-    var tracker = root[props.shift()];
+module.exports = function (root, children) {
+  var props = children.split('.');
+  var tracker = root[props.shift()];
 
-    while (props.length) {
-      if (tracker.hasOwnProperty(props[0])) {
-        tracker = tracker[props.shift()];
-      } else {
-        return;
-      }
+  while (props.length) {
+    if (tracker.hasOwnProperty(props[0])) {
+      tracker = tracker[props.shift()];
+    } else {
+      return;
     }
-    return tracker;
   }
-};
+  return tracker;
+}
