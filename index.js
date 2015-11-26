f4cc6ad705bfdd17406be08206aef2eb6e71bded
Frank van Wijk <fvanwijk@xebia.com>
2015-11-26 21:46:28 +0100

    refactor: Use reduce instead of while loop

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ module.exports = function (root, children) {
   var props = children.split('.');
   var tracker = root[props.shift()];
 
-  while (props.length) {
-    if (tracker.hasOwnProperty(props[0])) {
-      tracker = tracker[props.shift()];
-    } else {
-      return;
-    }
-  }
-  return tracker;
-}
+  return props.reduce(function (acc, prop) {
+    return acc.hasOwnProperty(prop) ? acc[prop] : undefined;
+  }, tracker);
+};
