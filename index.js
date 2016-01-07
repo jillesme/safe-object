a0f1795b034ed0f0a5a6a2d123890a2f49732725
Jilles Soeters <jsoeters@xebia.com>
2016-01-07 09:35:22 +0100

    Formatting, well done GitHub

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = function (root, children) {
   var props = children.split('.');
   var tracker = root[props.shift()];
-  
+
   do {
-  	if (!tracker.hasOwnProperty(props[0])) break;
+    if (!tracker.hasOwnProperty(props[0])) break;
     tracker = tracker[props.shift()];
   } while(props.length);
-  
+
   return tracker;
-}
+};
